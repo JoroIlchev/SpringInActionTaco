e33e52c2037d7437cd69359a0fe641b74711ecb2
GeorgiIlchev <georgi.ilchev@yahoo.com>
2020-06-22 11:37:03 +0300

    Spring JPA repos, not using JDBC impl of repos

--- src/main/java/tacos/Order.java
+++ src/main/java/tacos/Order.java
@@ -3,17 +3,22 @@ package tacos;
 import lombok.Data;
 import org.hibernate.validator.constraints.CreditCardNumber;
 
-import javax.persistence.PrePersist;
+import javax.persistence.*;
 import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 @Data
-public class Order {
+@Entity
+@Table(name = "Taco_Order")
+public class Order implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     private Date placedAt;
@@ -34,7 +39,7 @@ public class Order {
     @NotBlank(message="Zip code is required")
     private String zip;
 
-//    @CreditCardNumber(message="Not a valid credit card number")
+   @CreditCardNumber(message="Not a valid credit card number")
     private String ccNumber;
 
     @Pattern(regexp="^(0[1-9]|1[0-2])([\\/])([1-9][0-9])$",
@@ -44,6 +49,7 @@ public class Order {
     @Digits(integer=3, fraction=0, message="Invalid CVV")
     private String ccCVV;
 
+    @ManyToMany(targetEntity = Taco.class)
     private List<Taco> tacos = new ArrayList<>();
 
     public void addTaco(Taco design) {
@@ -54,8 +60,8 @@ public class Order {
         return new ArrayList<>(this.tacos);
     }
 
-//    @PrePersist
-//    void placedAt() {
-//        this.placedAt = new Date();
-//    }
+    @PrePersist
+    void placedAt() {
+        this.placedAt = new Date();
+    }
 }
--- src/main/java/tacos/data/OrderRepository.java
+++ src/main/java/tacos/data/OrderRepository.java
@@ -1,8 +1,15 @@
 package tacos.data;
 
+import org.springframework.data.repository.CrudRepository;
 import tacos.Order;
 
-public interface OrderRepository {
+import java.util.Date;
+import java.util.List;
+
+public interface OrderRepository extends CrudRepository<Order, String> {
+
+    List<Order> findAllByZip(String zipCode);
+    List<Order> findAllByZipAndPlacedAtBetween(String zipCode, Date startDate, Date endDate);
 
     Order save(Order order);
 }
